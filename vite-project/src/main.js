b9b05a5a7f3d2481ee45e62b0f770eb51fc762d4
Juiceman78 <vincentz78@nycstudents.net>
2025-11-13 13:59:39 -0500

    You don't know about him?????

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -3,26 +3,6 @@ import javascriptLogo from "./javascript.svg";
 import viteLogo from "/vite.svg";
 import { setupCounter } from "./counter.js";
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
-
-setupCounter(document.querySelector("#counter"));
-
 const songs = [
   { title: "Lose Control", artist: "Teddy Swims" },
   { title: "Beautiful Things", artist: "Benson Boone" },
@@ -61,14 +41,35 @@ const songs = [
   { title: "Uptown Funk", artist: "Mark Ronson ft. Bruno Mars" },
 ];
 
-function inject() {
-  const container = document.querySelector(".app");
+function inject(item) {
+  container = document.querySelector("#app");
   container.insertAdjacentHTML(
-    "afterbegin",
-    `<div class="card">
-        <h2 class="title">${item.title}</h2>
-        <h2 class="artist">${item.artist}</h2>
-      </div> `
+    "Afterbegin",
+    `<div class ="card">
+    <h2 class ="title">${item - title}</h2>
+    <h2 class ="artist">${item - artist}</h2>
+    </div>`
   );
 }
-inject();
+songs.forEach((element) => {
+  inject(element);
+});
+/* document.querySelector("#app").innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`;
+
+setupCounter(document.querySelector("#counter")); */
