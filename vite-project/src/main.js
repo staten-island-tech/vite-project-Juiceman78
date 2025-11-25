c1ff36369055ee5c25c5c6145f6ebbe44f51898f
Juiceman78 <vincentz78@nycstudents.net>
2025-11-25 13:59:45 -0500

    Callitaday

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -49,3 +49,12 @@ function inject(songs) {
 }
 inject(songs);
 
+document.querySelector(".mode").addEventListener("click", function () {
+  if (document.body.classList.contains("light")) {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+  } else {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+  }
+});
